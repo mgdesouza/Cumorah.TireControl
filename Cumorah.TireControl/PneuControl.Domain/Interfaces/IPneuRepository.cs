5540c6651b93564e785a061dcaf9377b98eadd96
Marcelo.Souza <33257503+mgdesouza@users.noreply.github.com>
2026-08-25 13:22:29 -0300

    Delete Cumorah.TireControl/PneuControl.Domain directory

--- Cumorah.TireControl/PneuControl.Domain/Interfaces/IPneuRepository.cs
+++
@@ -1,8 +0,0 @@
-namespace PneuControl.Domain.Interfaces;
-
-using PneuControl.Domain.Entities;
-
-public interface IPneuRepository : IRepository<Pneu>
-{
-    Task<IEnumerable<Pneu>> GetByVeiculoIdAsync(Guid veiculoId);
-}
