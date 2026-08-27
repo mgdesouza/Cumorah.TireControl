23c4f150e4296ef1b77f919097b8ff5a0a4c917f
Marcelo.Souza <33257503+mgdesouza@users.noreply.github.com>
2026-08-27 16:45:45 -0300

    feat(auth): add authorization role constants

---
+++ Cumorah.TireControl/TireControl.Infrastructure/Data/AuthorizationRoles.cs
@@ -0,0 +1,9 @@
+namespace TireControl.Infrastructure.Data;
+
+public static class AuthorizationRoles
+{
+    public const string AdministradorSistema = "AdministradorSistema";
+    public const string Suporte = "Suporte";
+    public const string AdministradorCliente = "AdministradorCliente";
+    public const string UsuarioCliente = "UsuarioCliente";
+}
